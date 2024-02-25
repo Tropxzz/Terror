c3e40c40c2fe64cb78e7c82155937e0875fc4ebd
Tropxzz <130433432+Tropxzz@users.noreply.github.com>
2024-02-25 16:24:41 +1100

    Update Breakin2.lua

--- Games/Breakin2.lua
+++ Games/Breakin2.lua
@@ -203,6 +203,22 @@ Ingame:AddButton({
 	end,
 })
 
+Ingame:AddButton({
+        Title = "Get Golden Apple",
+        Description = 'Free GoldenApple fr.',
+        Callback = function()
+              		local function GiveItem(Item)
+			if Item == "Armor" then
+				Events:WaitForChild("Vending"):FireServer(3, "Armor2", "Armor", tostring(player), 1)
+			else
+				Events:WaitForChild("GiveTool"):FireServer(tostring(Item:gsub(" ", "")))
+			end
+		end
+
+		GiveItem("Golden Apple") -- rizz bros
+	end,
+})
+
 -- settings
 
 	InterfaceManager:SetLibrary(Fluent)
