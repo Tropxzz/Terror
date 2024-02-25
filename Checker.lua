dd6a22f2f4594adfbc58d6b80d197c7ed12690f7
Tropxzz <130433432+Tropxzz@users.noreply.github.com>
2024-02-25 15:57:26 +1100

    Update Checker.lua

--- Checker.lua
+++ Checker.lua
@@ -1,3 +1,6 @@
+queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+
+
 if game.PlaceId == 13864667823 or game.PlaceId == 13864661000 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/Terror/main/Games/Breakin2.lua", true))()
 elseif game.PlaceId == "67891" then
@@ -6,4 +9,4 @@ else
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/Terror/main/Universal.lua", true))()
 end
 
--- e.g
+queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Tropxzz/Terror/main/Checker.lua', true))()")
