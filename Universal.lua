2ef54cd873513d3371a7d04f97aa1e592155264b
Tropxzz <130433432+Tropxzz@users.noreply.github.com>
2024-02-26 19:04:37 +1100

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -1,298 +1,259 @@
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
-local Dialog = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/Terror/main/Modules/TerrorDialog.lua", true))()
-local Events = game:GetService("ReplicatedStorage"):WaitForChild("Events")
+			
+		-- ui varibles
+			local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+			local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+			local ESPModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/Terror/main/Modules/ESPModule.lua", true))()
+			local AimbotModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/Terror/main/Modules/Aimbot.lua", true))()
+			local Dialog = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/Terror/main/Modules/TerrorDialog.lua", true))()
+			local HTTPRequest = (fluxus and fluxus.request) or (syn and syn.request) or (http and http.request) or http_request or request
+			local hook = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/Terror/main/Modules/Hook.lua", true))()
+-- varibles
+			local PlaceId = game.PlaceId
+local JobId = game.JobId
+local vars = {}
 
-local player = game.Players.LocalPlayer
 
-local Window = Fluent:CreateWindow({
-		Title = "Terror 1.0 - Break in 2",
-		SubTitle = "by Jimmy/Tropxz",
-		TabWidth = 160,
-		Size = UDim2.fromOffset(580, 460),
-		Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
-		Theme = "Dark",
-		MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
-})
 
-local Lobby = Window:AddTab({ Title = "Lobby", Icon = "egg" })
-local Ingame = Window:AddTab({ Title = "Ingame", Icon = "egg" })
-local Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+			local player = game.Players.LocalPlayer
+			-- main script
 
-function TeleportTo(c)
-	player.Character.HumanoidRootPart.CFrame = c
-end
+			local Window = Fluent:CreateWindow({
+				Title = "Terror 1.0",
+				SubTitle = "by Jimmy/Tropxz",
+				TabWidth = 160,
+				Size = UDim2.fromOffset(580, 460),
+				Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+				Theme = "Dark",
+				MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+			})
+
+				Main = Window:AddTab({ Title = "Welcome", Icon = "home" })
+				Plr = Window:AddTab({ Title = "Player", Icon = "user" })
+				Combat = Window:AddTab	({ Title = "Combat", Icon = "swords" })
+				Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+
 
 Window:SelectTab(1)
 
--- Lobby
 
-   Lobby:AddParagraph({
-        Title = "Section 1",
-	Content = "This section contains Role Exploits"
+			  Main:AddParagraph({
+				Title = "Welcome",
+				Content = "Welcome "..game.Players.LocalPlayer.DisplayName.." This script was made By Tropxz/Jimmy Were the same person if u couldnt tell"
+			})
+
+			Main:AddButton({
+				Title = "Rejoin",
+				Description = "Rejoin the server.",
+				Callback = function()
+					game["Teleport Service"]:TeleportToPlaceInstance(PlaceId, JobId, player)
+				end
 })
 
- Lobby:AddButton({
-        Title = "Nerd",
-        Description = 'Makes ur Role "Nerd".',
-        Callback = function()
-           local A_1 = "Book"
-		if game:GetService("ReplicatedStorage").RemoteEvents:FindFirstChild("OutsideRole") ~= nil  then
-			local Event = game:GetService("ReplicatedStorage"):FindFirstChild("RemoteEvents"):FindFirstChild("OutsideRole")
-			Event:FireServer(A_1)
-		end
-        end
-    })
-
-
-Lobby:AddButton({
-        Title = "Hacker",
-        Description = 'Makes ur Role "Hacker".',
-        Callback = function()
-           local A_1 = "Phone"
-		if game:GetService("ReplicatedStorage").RemoteEvents:FindFirstChild("OutsideRole") ~= nil  then
-			local Event = game:GetService("ReplicatedStorage"):FindFirstChild("RemoteEvents"):FindFirstChild("OutsideRole")
-			Event:FireServer(A_1)
-		end
-        end
-    })
-
-
-
--- Ingame
-
-   Ingame:AddParagraph({
-        Title = "Section 1",
-	Content = "This section contains Speed/Strength Exploits"
-})
-
-Ingame:AddButton({
-        Title = "Max Speed",
-        Description = 'Maxes out your speed.',
-        Callback = function()
-          	local function Train(Ability)
-			Events:WaitForChild("RainbowWhatStat"):FireServer(Ability)
-		end
-		local a = 0
-		while wait(1) do
-			a = a + 1
-			if a ~= 5 then
-				Train("Strength")
+Main:AddButton({
+		Title = "Screenshot",
+		Description = "Takes a screenshot.",
+		Callback = function()
+				return game.CoreGui:TakeScreenshot()
 			end
-		end
-	end,
-})
+		})
 
-
-
-Ingame:AddButton({
-        Title = "Max Strength",
-        Description = 'Maxes out ur strength.',
-        Callback = function()
-               	local function Train(Ability)
-			Events:WaitForChild("RainbowWhatStat"):FireServer(Ability)
-		end
-		local a = 0
-		while wait(1) do
-			a = a + 1
-			if a ~= 5 then
-				Train("Speed")
+		Main:AddButton({
+			Title = "Serverhop",
+			Description = "Join another server easily.",
+			Callback = function()
+				game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceId, player)
 			end
-		end
-	end,
 })
 
-Ingame:AddButton({
-        Title = "+1 Strength",
-        Description = 'Adds one strength point.',
-        Callback = function()
-               	local function Train(Ability)
-			Events:WaitForChild("RainbowWhatStat"):FireServer(Ability)
-		end
-		Train("Strength")
-	end,
-})
 
-Ingame:AddButton({
-        Title = "+1 Speed",
-        Description = 'Adds one Speed point.',
-        Callback = function()
-               	local function Train(Ability)
-			Events:WaitForChild("RainbowWhatStat"):FireServer(Ability)
-		end
-		Train("Speed")
-	end,
-})
-
-   Ingame:AddParagraph({
-        Title = "Section 2",
-	Content = "This section contains  NPC Exploits"
-})
-
-Ingame:AddButton({
-	Title = "Get all NPCS",
-	Description = "gets all npcs.",
+Main:AddButton({
+	Title = "AntiClientKick",
+	Description = "Cant get kicked by the client. MADE BY IY",
 	Callback = function()
-		local function GiveItem(Item)
-			if Item == "Armor" then
-				Events:WaitForChild("Vending"):FireServer(3, "Armor2", "Armor", tostring(game.Players.LocalPlayer), 1)
-			else
-				Events:WaitForChild("GiveTool"):FireServer(tostring(Item:gsub(" ", "")))
+			if not hookmetamethod then 
+		return Dialog.red("Your executor doesn't support hookmetamethod")
+	end
+	local TeleportService = game["Teleport Service"]
+	local oldhmmi
+	local oldhmmnc
+	oldhmmi = hookmetamethod(game, "__index", function(self, method)
+		if self == TeleportService then
+			if method:lower() == "teleport" then
+				return error("Expected ':' not '.' calling member function Kick", 2)
+			elseif method == "TeleportToPlaceInstance" then
+				return error("Expected ':' not '.' calling member function TeleportToPlaceInstance", 2)
 			end
 		end
-		
-		local function GetDog()
-			for i, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Assets.Note.Note.Note:GetChildren()) do
-				if v.Name:match("Circle") and v.Visible == true then
-					GiveItem(tostring(v.Name:gsub("Circle", "")))
-					task.wait(.1)
-					player.Backpack:WaitForChild(tostring(v.Name:gsub("Circle", ""))).Parent = player.Character
-					TeleportTo(CFrame.new(-257.56839, 29.4499969, -910.452637, -0.238445505, 7.71292363e-09, 0.971155882, 1.2913591e-10, 1, -7.91029819e-09, -0.971155882, -1.76076387e-09, -0.238445505))
-					task.wait(.5)
-					Events:WaitForChild("CatFed"):FireServer(tostring(v.Name:gsub("Circle", "")))
-				end
-			end
-			task.wait(2)
-			for i = 1, 3 do
-				TeleportTo(CFrame.new(-203.533081, 30.4500484, -790.901428, -0.0148558766, 8.85941187e-09, -0.999889672, 2.65695732e-08, 1, 8.46563175e-09, 0.999889672, -2.64408779e-08, -0.0148558766) + Vector3.new(0, 5, 0))
-				task.wait(.1)
-			end
+		return oldhmmi(self, method)
+	end)
+	oldhmmnc = hookmetamethod(game, "__namecall", function(self, ...)
+		if self == TeleportService and getnamecallmethod():lower() == "teleport" or getnamecallmethod() == "TeleportToPlaceInstance" then
+			return
 		end
+		return oldhmmnc(self, ...)
+	end)
+	     end
+		})
 
-		local function GetAgent()
-			GiveItem("Louise")
-			task.wait(.1)
-			player.Backpack:WaitForChild("Louise").Parent = player.Character
-			Events:WaitForChild("LouiseGive"):FireServer(2)
-		end	
+		-- Player
 
-		local function GetUncle()
-			GiveItem("Key")
-			task.wait(.1)
-			player.Backpack:WaitForChild("Key").Parent = player.Character
-			wait(.5)
-			Events.KeyEvent:FireServer()
-		end
-		
-		GetUncle()
-		task.wait(3)
-		GetDog()
-		task.wait(3)
-		GetAgent()
-	end,
-})
-
-   Ingame:AddParagraph({
-        Title = "Section 3",
-	Content = "This section contains  item Exploits"
-})
---[[
-Ingame:AddButton({
-        Title = "Get armor",
-        Description = 'Free armor fr.',
-        Callback = function()
-              		local function GiveItem(Item)
-			if Item == "Armor" then
-				Events:WaitForChild("Vending"):FireServer(3, "Armor2", "Armor", tostring(player), 1)
-			else
-				Events:WaitForChild("GiveTool"):FireServer(tostring(Item:gsub(" ", "")))
+		Plr:AddSlider("Ws", {
+			Title = "WalkSpeed",
+			Description = "Speeds up your Player",
+			Default = 16,
+			Min = 16,
+			Max = 1000,
+			Rounding = 1,
+			Callback = function(Value)
+				player.Character.Humanoid.WalkSpeed = Value
 			end
-		end
+		});
 
-		GiveItem("Armor") -- rizz bros
-	end,
-})
-
-Ingame:AddButton({
-        Title = "Get Golden Apple",
-        Description = 'Free GoldenApple fr.',
-        Callback = function()
-              		local function GiveItem(Item)
-			if Item == "Armor" then
-				Events:WaitForChild("Vending"):FireServer(3, "Armor2", "Armor", tostring(player), 1)
-			else
-				Events:WaitForChild("GiveTool"):FireServer(tostring(Item:gsub(" ", "")))
+		Plr:AddSlider("JP", {
+			Title = "JUMPPOWER",
+			Description = "Makes your character Jump Higher",
+			Default = 50,
+			Min = 50,
+			Max = 1000,
+			Rounding = 1,
+			Callback = function(Value)
+				player.Character.Humanoid.JumpPower = Value
 			end
-		end
+		});
 
-		GiveItem("Golden Apple") -- rizz bros
-	end,
-})
-
-Ingame:AddButton({
-        Title = "Get GOlden pizza",
-        Description = 'Free Health  fr.',
-        Callback = function()
-	              		local function GiveItem(Item)
-				if Item == "Armor" then
-					Events:WaitForChild("Vending"):FireServer(3, "Armor2", "Armor", tostring(player), 1)
-				else
-					Events:WaitForChild("GiveTool"):FireServer(tostring(Item:gsub(" ", "")))
-				end
+		Plr:AddSlider("HipHeight", {
+			Title = "HipHeight",
+			Description = "Makes your character Float",
+			Default = 1.9980254173278809,
+			Min = 1.9980254173278809,
+			Max = 250,
+			Rounding = 1,
+			Callback = function(Value)
+				player.Character.Humanoid.HipHeight = Value
 			end
+});
 
-			GiveItem("Gold Pizza")
-	end,
-})
-]]
+local spinSpeed = 1
+local s = Plr:AddToggle("Spin", {Title = "Spin", Default = false })
 
-    local Weapons = Ingame:AddDropdown("Dropdown", {
-        Title = "Weapons",
-        Values = {"PitchFork"},
-        Multi = false,
-        Default = "PitchFork",
-})
-
-
-   Weapons:OnChanged(function(Value)
-	-- if the github account hello-n-bye steals this im suing 😎 HE FUCKING MADE THE 0 DOLLAR IDEA
-	local args = {
-    [1] = 3,
-    [2] = Value,
-    [3] = "Weapons",
-    [4] = "jopzllwastaken",
-    [6] = 0
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Vending"):FireServer(unpack(args))
+s:OnChanged(function()
+	if s.Value == true then
+		 for i,v in pairs(game.Players.LocalPlayer.Character.HumanoidRootPart:GetChildren()) do
+		if v.Name == "Spinning" then
+			v:Destroy()
+		end
+	end
+	local Spin = Instance.new("BodyAngularVelocity")
+	Spin.Name = "Spinning"
+	Spin.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
+	Spin.MaxTorque = Vector3.new(0, math.huge, 0)
+		Spin.AngularVelocity = Vector3.new(0,spinSpeed,0)
+	else
+		for i,v in pairs(game.Players.LocalPlayer.Character.HumanoidRootPart:GetChildren()) do
+		if v.Name == "Spinning" then
+			v:Destroy()
+		end
+	end
+	end
     end)
 
-local Items = Ingame:AddDropdown("Items", {
-        Title = "Items",
-        Values = {"Gold Pizza"},
-        Multi = false,
-        Default = 1,
+    local Input = Plr:AddInput("SpinSpeed", {
+        Title = "SpinSpeed",
+        Default = "1",
+        Placeholder = "1",
+        Numeric = true, -- Only allows numbers
+        Finished = false, -- Only calls callback when you press enter
+        Callback = function(Value)
+                    spinSpeed = Value
+        end
+    })
+
+
+
+
+
+		-- Combat
+
+
+		local ESP = Combat:AddToggle("ESP", {Title = "ESP", Default = false })
+
+		ESP:OnChanged(function()
+			if ESP.Value == false  then
+				ESPModule.DisableESP()
+			elseif ESP.Value == true  then
+				ESPModule.ShowESP()
+			end
+		end)
+		-- lesson learnt never add a debounce to a toggle in a ui library lmfao (fyi it broke)
+		-- Alr hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm lets do 👑 Aimbot 👑
+		-- yo bros my old module things got me covered :fire:
+		-- ima just upload it to my github and were done basically
+
+		Combat:AddButton({
+			Title = "Aimbot",
+			Description = "Automatically aims when u hold RB.",
+			Callback = function()
+				AimbotModule.StartAimbot()
+			end
+		})
+		_G.TracersVisible = false
+		local Toggle = Combat:AddToggle("Tracers by Exunys", {Title = "Tracers by Exunys", Default = false })
+		local tracescriptenabled = false
+		Toggle:OnChanged(function()
+			if tracescriptenabled == false then
+				loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/Terror/main/ExternalScripts/EnuxysTracers", true))()
+				tracescriptenabled = true
+			end
+			if Toggle.Value == false then
+				_G.TracersVisible = false
+			else
+				_G.TracersVisible = true
+			end
+		end)
+
+
+		local Noclip = Combat:AddToggle("Noclip", {Title = "Noclip", Default = false })
+
+		Noclip:OnChanged(function()
+			for i,v in ipairs(player.Character:GetDescendants()) do
+				if v:IsA("MeshPart") or v:IsA("Part") then
+					if Noclip.Value == false  then
+		              v.CanCollide = true
+					elseif Noclip.Value == true  then
+						v.CanCollide = false
+					end
+				end
+			end
+end)
+
+local Spectate = Combat:AddDropdown("Spectate", {
+	Title = "Spectate",
+	Values = {},
+	Multi = false,
+	Default = player.Name,
 })
 
-  Items:OnChanged(function(Value)
- 		local function GiveItem(Item)
-			if Item == "Armor" then
-				Events:WaitForChild("Vending"):FireServer(3, "Armor2", "Armor", tostring(player), 1)
-			else
-				Events:WaitForChild("GiveTool"):FireServer(tostring(Item:gsub(" ", "")))
-			end
+local names = {}
+local players = game.Players:GetPlayers()
+for i, player in ipairs(players) do
+	table.insert(names, player.Name)
+end
+	Spectate:SetValues(names)
+	Spectate:OnChanged(function(Value)
+		if game.Players:FindFirstChild(Value) then
+			workspace.CurrentCamera.CameraSubject = game.Players[Value].Character
 		end
+end)
 
-		GiveItem(Value)
-    end)
+-- interface settings
+			InterfaceManager:SetLibrary(Fluent)
+			InterfaceManager:BuildInterfaceSection(Settings)
 
-local core = game:GetService("CoreGui")
-for _,v in ipairs(core:GetDescendants()) do
-if v.ClassName == "TextLabel" then
-if v.Text == "Weapons" or v.Text == "Items" then
-local newLabel = v.Parent.Parent.TextButton.TextLabel
-newLabel.Text = "Select One"
-end
-end
-end
+
+		-- put code above this not under 👑😭🙏 
 
 
 
--- settings
-
-	InterfaceManager:SetLibrary(Fluent)
-InterfaceManager:BuildInterfaceSection(Settings)
-
-	Dialog.yellow("The Script Terror has loaded")
-
-
+			Dialog.yellow("The Script Terror has loaded")
+		
+		
