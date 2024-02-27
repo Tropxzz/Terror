ddd624b3ad2c1e978ba8f258b858907f16b5344c
Tropxzz <130433432+Tropxzz@users.noreply.github.com>
2024-02-27 16:00:51 +1100

    Update Breakin2.lua

--- Games/Breakin2.lua
+++ Games/Breakin2.lua
@@ -221,7 +221,7 @@ if game.PlaceId == 13864667823 then
     for _, v in pairs(game.ReplicatedStorage.Vending.Weapons:GetDescendants()) do
         if v:IsA("Model") then
             table.insert(Weapons.Values, v.Name)
-        end
+		end
     end
 end
 
