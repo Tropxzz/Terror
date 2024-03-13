356d97d6021a789608149f73bdfc92ff7241f6be
Tropxzz <130433432+Tropxzz@users.noreply.github.com>
2024-03-13 17:50:15 +1100

    Update Checker.lua

--- Checker.lua
+++ Checker.lua
@@ -1,14 +1,13 @@
 queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 
-
-if game.PlaceId == 13864667823  then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/Terror/main/Games/Breakin2.lua", true))()
+if game.PlaceId == 13864667823 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/Terror/main/Games/Breakin2.lua", true))()
 elseif game.PlaceId == 13864661000 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/Terror/main/Games/breakin2lobby.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/Terror/main/Games/breakin2lobby.lua",true))()
 elseif game.PlaceId == 3956818381 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/Terror/main/Games/NinjaLegends.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/Terror/main/Games/NinjaLegends.lua", true))()
 else
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/Terror/main/Universal.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/Terror/main/Universal.lua", true))()
 end
 
 -- queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Tropxzz/Terror/main/Checker.lua', true))()")
